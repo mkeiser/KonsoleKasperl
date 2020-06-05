2f2fc0432dea558a872dd683e0c1a358a2cee8b3
Matthias Keiser <matthias@tristan-inc.com>
2020-06-05 18:36:07 +0200

    Initial Commit

---
+++ Conman/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Conman
+//
+//  Created by Matthias Keiser on 05/06/2020.
+//  Copyright © 2020 Matthias Keiser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
